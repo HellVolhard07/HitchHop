245220c3f8d0f548daac605b284370f6dbb9f7e5
PulkitAsri <pulkit.asri@gmail.com>
2023-06-20 12:38:41 +0530

    password input

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         useMaterial3: true,
       ),
-      initialRoute: availableCarsScreen,
+      initialRoute: loginScreen,
       // home: MapSample(type: 'give'),
       routes: {
         signupScreen: (_) => SignupScreen(),
