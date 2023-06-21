64c7ad73e2f485e2346a6cbe8807a6ccd42dae01
PulkitAsri <pulkit.asri@gmail.com>
2023-06-22 04:18:20 +0530

    location fixed

--- lib/screens/landingScreen.dart
+++ lib/screens/landingScreen.dart
@@ -2,23 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:hitchhop/constants.dart';
 import 'package:hitchhop/screens/mapSample.dart';
 import 'package:hitchhop/widgets/availableCarsScreen/rideTypeTile.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class LandingScreen extends StatelessWidget {
   const LandingScreen({Key? key}) : super(key: key);
-
-  // void giveOnTap(BuildContext context) {
-  //   Navigator.push(
-  //     context,
-  //     MaterialPageRoute(builder: (context) => MapSample()),
-  //   );
-  // }
-  //
-  // void takeOnTap(BuildContext context) {
-  //   Navigator.push(
-  //     context,
-  //     MaterialPageRoute(builder: (context) => MapSample()),
-  //   );
-  // }
 
   @override
   Widget build(BuildContext context) {
